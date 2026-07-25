4dbe1aa1d3c64d23089a902c99a598b592fd11d2
RicherdDaniel13 <144140906+RicherdDaniel13@users.noreply.github.com>
2026-07-25 20:56:49 +0530

    Update index.js

--- project-1-freestyle/index.js
+++ project-1-freestyle/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 
 app.get("/", (req, res) => {
